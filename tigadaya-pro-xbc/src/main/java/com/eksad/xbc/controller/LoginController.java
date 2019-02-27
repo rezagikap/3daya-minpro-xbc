a763c803652b5262f1595fb3e52225d1fcdf89fc
Saqiraz Nurbaitil Atik Cahyati <atikcahyati21@gmail.com>
2019-02-27 22:19:17 +0700

    tambah log in

---
+++ tigadaya-pro-xbc/src/main/java/com/eksad/xbc/controller/LoginController.java
@@ -0,0 +1,23 @@
+package com.eksad.xbc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping("/login.html")
+	public String loginForm() {
+		return "login";
+	}
+
+	@RequestMapping("/home.html")
+	public String home() {
+		return "home";
+	}
+
+	@RequestMapping("/403.html")
+	public String accessDenied() {
+		return "403";
+}
+}
