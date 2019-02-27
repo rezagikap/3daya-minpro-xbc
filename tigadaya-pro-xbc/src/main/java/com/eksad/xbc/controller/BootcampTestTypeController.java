81d9ca1762c4fba8ee2f3a0cb3c4f9e25bb6aff3
Siska Wahyu Oktavia <siskaoktavia215@gmail.com>
2019-02-27 19:27:39 +0700

    update

--- tigadaya-pro-xbc/src/main/java/com/eksad/xbc/controller/BootcampTestTypeController.java
+++ tigadaya-pro-xbc/src/main/java/com/eksad/xbc/controller/BootcampTestTypeController.java
@@ -1,18 +1,30 @@
 package com.eksad.xbc.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.eksad.xbc.service.BootcampTestTypeService;
+
 @Controller
 public class BootcampTestTypeController {
+	private Log log = LogFactory.getLog(getClass());
+	
+	@Autowired
+	private BootcampTestTypeService service;
 	
 	@RequestMapping(value="/bootcamptesttype")
 	public String index() {
 		return "/bootcamptesttype/index";
 	}
 	
-	// digunakan untuk URL nya
-	// menampilkan bootcamptesttype create.jsp
+	@RequestMapping(value="/bootcamptesttype/detail")
+	public String detail(){
+		return "/bootcamptesttype/detail";
+	}
+	
 	@RequestMapping(value="/bootcamptesttype/create")
 	public String create(){
 		return "/bootcamptesttype/create";
